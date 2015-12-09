bd2a4fcd0a9537d104b5cbcd58e1d3293c709447
Gumbrat <calebcrawford29@gmail.com>
2015-12-09 10:54:12 -0600

    Update HungerSystem.php

--- src/PlayHarder/system/HungerSystem.php
+++ src/PlayHarder/system/HungerSystem.php
@@ -7,14 +7,14 @@ use pocketmine\Player;
 use PlayHarder\attribute\AttributeProvider;
 
 class HungerSystem {
-	const WALKING_AND_SNEAKING = 0.005;
-	const SWIMMING = 0.015;
-	const BREAKING_A_BLOCK = 0.025;
-	const SPRINTING = 0.1;
-	const JUMPING = 0.1;
-	const ATTACKING_AN_ENEMY = 0.3;
-	const RECEIVING_ANY_DAMAGE = 0.3;
-	const JUMPING_WHILE_SPRINTING = 0.4;
+	const WALKING_AND_SNEAKING = 0.001;
+	const SWIMMING = 0.001;
+	const BREAKING_A_BLOCK = 0.001;
+	const SPRINTING = 0.001;
+	const JUMPING = 0.001;
+	const ATTACKING_AN_ENEMY = 0.001;
+	const RECEIVING_ANY_DAMAGE = 0.001;
+	const JUMPING_WHILE_SPRINTING = 0.001;
 	public static function exhaustion(Player $player, $point) {
 		$attribute = AttributeProvider::getInstance ()->getAttribute ( $player );
 		$attribute->setHunger ( $attribute->getHunger () - $point );
